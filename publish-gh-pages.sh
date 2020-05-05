a5b9095fb701302f6f64ca4111733986d42ccb59
Myles Bostwick <github@zithora.com>
2020-05-05 15:45:34 -0600

    Fix publish script

--- publish-gh-pages.sh
+++ publish-gh-pages.sh
@@ -3,10 +3,10 @@
 # 1. Set up a worktree in directory dist checked out on branch gh-pages
 git worktree add dist/timezone-picker-runner gh-pages
 # 2. Build whatever needs to be in dist however you like
-ng build --baseHref=/ng-timezone-picker/ --prod timezone-picker-runner
+ng build --baseHref=/ng-timezone-picker/ timezone-picker-runner
 # 3. Add everything in dist
 (cd dist/timezone-picker-runner; git add .)
 # 4. Commit, with some nice message to link to sources commit
-(cd dist/timezone-picker-runner; git commit -m "Build output as of $(git log '--format=format:%H' master -1))"
+(cd dist/timezone-picker-runner; git commit -m "Build output as of $(git log '--format=format:%H' master -1)")
 # 5. Push
-git push origin gh-page
+git push origin gh-pages
