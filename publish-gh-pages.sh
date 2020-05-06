b4ac41b41505f943246f42377b634ecd747d81df
Myles Bostwick <github@zithora.com>
2020-05-06 10:15:30 -0600

    This one actually works because dist folder doesn't get deleted on build

--- publish-gh-pages.sh
+++ publish-gh-pages.sh
@@ -2,12 +2,12 @@
 
 rm -rf dist
 # 1. Set up a worktree in directory dist checked out on branch gh-pages
-git worktree add dist/timezone-picker-runner gh-pages
+git worktree add -f dist gh-pages
 # 2. Build whatever needs to be in dist however you like
-ng build --baseHref=/ng-timezone-picker/ timezone-picker-runner
+ng build timezone-map-picker && ng build timezone-ng-select-picker && ng build --baseHref=/ng-timezone-picker/timezone-picker-runner timezone-picker-runner
 # 3. Add everything in dist
-(cd dist/timezone-picker-runner; git add .)
+(cd dist; git add .)
 # 4. Commit, with some nice message to link to sources commit
-(cd dist/timezone-picker-runner; git commit -m "Build output as of $(git log '--format=format:%H' master -1)")
+(cd dist; git commit -m "Build output as of $(git log '--format=format:%H' master -1)")
 # 5. Push
 git push origin gh-pages
